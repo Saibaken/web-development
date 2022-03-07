1d12d7c08585a35998a1f46946807ebdae2933ff
Жугинский Иван <zhuga200117@gmail.com>
2022-03-07 13:00:15 +0300

    fix: :lipstick: Исправил поведение кнопок меню
    Теперь кликабельна вся кнопка, а не только текст

--- group-chat/src/bits/SideLink.js
+++ group-chat/src/bits/SideLink.js
@@ -6,20 +6,20 @@ import { MdPerson, MdEmail, MdFeed } from "react-icons/md";
 export default function SideLink(props) {
   return (
     <li className="side-link">
-      {(() => {
-        switch (props.link) {
-          case "/news":
-            return <MdFeed />;
-          case "/chat":
-            return <MdEmail />;
-          case "/my_profile":
-            return <MdPerson />;
-          default :
-            return <></>
-        }
-      })()}
       <Link to={props.link} className="side-link-text">
-        {props.text}
+          {(() => {
+            switch (props.link) {
+              case "/news":
+                return <MdFeed />;
+              case "/chat":
+                return <MdEmail />;
+              case "/my_profile":
+                return <MdPerson />;
+              default:
+                return <></>
+            }
+          })()}
+          {props.text}
       </Link>
     </li>
   );
