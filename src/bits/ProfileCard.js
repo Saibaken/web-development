3140f9e96fd6154e991ecc3c1589fc78886bc417
Ivan Zhuginsky <Zhuga200117@gmail.com>
2022-03-09 22:42:24 +0300

    feat: :sparkles: Добавил реализацию выхода из аккаунта

--- src/bits/ProfileCard.js
+++ src/bits/ProfileCard.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ProfileCard() {
+  const navigate = useNavigate();
+
+  function onLogOut() {
+    localStorage.setItem("id", JSON.stringify(null));
+    navigate("/");
+  }
+
   return (
     <div className="profile-window">
       <div className="profile-info">
@@ -14,7 +22,7 @@ export default function ProfileCard() {
         <h3 className="profile-name">Profile Name</h3>
 
       </div>
-      <button className="log-out-button">
+      <button className="log-out-button" onClick={onLogOut}>
         Log Out
       </button>
     </div>
