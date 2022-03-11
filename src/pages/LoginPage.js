efeceea390748982bf117c55ed5a423348af4499
Жугинский Иван <zhuga200117@gmail.com>
2022-03-11 17:34:14 +0300

    feat: :sparkles: Написал валидацию полей логина
    Доп. изменения:
    
    - Заменил login на guid при переходе на страницу пользователя
    - Изменил тип инпута с text на email

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,30 +1,70 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { v4 } from 'uuid'
 import '../stylesheets/LoginPage.css'
 
 export default function LoginPage() {
 
-    let navigate = useNavigate();
+    let navigate = useNavigate()
 
-    // TODO: Написать валидацию полей
-    function validate() {
-        let login = document.getElementById('POST-login').value;
+    function validate(e) {
+        const name = e.target.name
 
-        if (login !== null) {
-            localStorage.setItem("id", JSON.stringify(login))
-            navigate("/" + login + "/news");
+        switch (name) {
+            case 'login': validateLogin(e.target)
+                break
+            case 'password': validatePassword(e.target)
+                break
+            default:
+                return
         }
     }
 
+    function validateLogin(field) {
+        if (field.validity.typeMismatch) {
+            field.setCustomValidity('Must be email')
+        }
+        else if (field.validity.valueMissing) {
+            field.setCustomValidity('Must not be empty')
+        }
+        else {
+            field.setCustomValidity('')
+        }
+    }
+
+    function validatePassword(field) {
+        if (field.validity.valueMissing) {
+            field.setCustomValidity('Must not be empty')
+        }
+        else if (field.validity.patternMismatch) {
+            field.setCustomValidity('A password must contain at least ' +
+                '1 lower case letter, 1 upper case letter and 1 number')
+        }
+        else if (field.validity.tooShort) {
+            field.setCustomValidity('A password must contain at least 8 symbols long')
+        }
+        else {
+            field.setCustomValidity('')
+        }
+    }
+
+    function sendForm(e) {
+        e.preventDefault()
+        
+        localStorage.setItem('id', JSON.stringify(v4()))
+        navigate('/' + v4() + '/news')
+    }
+
     return (
-        <form className='login-form' onSubmit={validate}>
+        <form className='login-form' onSubmit={(event) => sendForm(event)} onInput={(event) => validate(event)} >
             <label>
                 Login
-                <input id='POST-login' type='text' name='login' />
+                <input required id='POST-login' type='email' name='login' />
             </label>
             <label>
                 Password
-                <input id='POST-password' type='password' name='password' />
+                <input required id='POST-password' type='password' name='password'
+                    minLength={8} pattern='^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{0,}$' />
             </label>
             <input type='submit' value='Login'></input>
             <Link to='/registration'>Create an account</Link>
