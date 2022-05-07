5f910d3d2d03b339f377a6473ad4f651f24e6a1a
Saibaken <shelamov.s@gmail.com>
2022-05-07 10:47:07 +0300

    feat: :memo: Добавил и имплементировал переводы для страниц чата, новостей и профиля

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,10 +1,11 @@
 import "../stylesheets/Chat.css"
 import React, { useEffect, useState } from "react";
-import ChatWindow from "../bits/ChatWindow"
+import ChatWindow from "../bits/ChatWindow";
+import { useTranslation } from "react-i18next";
 
 export default function ChatPage() {
   const [id, setId] = useState(JSON.parse(localStorage.getItem("id")));
-
+  const { t } = useTranslation('pages');
   useEffect(() => {
     setId(JSON.parse(localStorage.getItem("id")));
   }, []);
@@ -12,7 +13,7 @@ export default function ChatPage() {
   if (id !== null)
     return (
       <div className="page-content">
-        <h1 className="page-header">Chat</h1>
+        <h1 className="page-header">{t("chat.chat_header")}</h1>
         <ChatWindow />
       </div>
     );
--- src/pages/NewsPage.js
+++ src/pages/NewsPage.js
@@ -1,11 +1,12 @@
 import "../stylesheets/News.css";
 import React, { useEffect, useState } from "react";
 import NewsCard from "../bits/NewsCard";
+import { useTranslation } from "react-i18next";
 
 
 function NewsPage() {
   const [id, setId] = useState(JSON.parse(localStorage.getItem("id")));
-
+  const { t } = useTranslation('pages');
   useEffect(() => {
     setId(JSON.parse(localStorage.getItem("id")));
   }, []);
@@ -13,7 +14,7 @@ function NewsPage() {
   if (id !== null)
     return (
       <div className="page-content">
-        <h1 className="page-header">News</h1>
+        <h1 className="page-header">{t("news.news_header")}</h1>
         <NewsCard video="true" />
         <NewsCard image="true" />
       </div>
